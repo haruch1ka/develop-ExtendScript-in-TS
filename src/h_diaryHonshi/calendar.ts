77a97539384ba6bd7255bfadf3b924704942a068
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-01-22 08:49:52 +0900

    diaryDayEntity、diaryInputData、diaryPageEntity、calendar、diaryPageStructureクラスを新規作成し、カレンダー機能を実装

---
+++ src/h_diaryHonshi/calendar.ts
@@ -0,0 +1,42 @@
+class calendar {
+	weekNameList = { "0": "日", "1": "月", "2": "火", "3": "水", "4": "木", "5": "金", "6": "土" };
+	monthDays: number[];
+	constructor() {
+		this.monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 月の日数
+	}
+	isLeapYear(year: number): boolean {
+		if (year % 400 === 0) return true;
+		if (year % 100 === 0) return false;
+		if (year % 4 === 0) return true;
+		return false;
+	}
+	changeLeapYear(year: number): void {
+		if (this.isLeapYear(year)) this.monthDays[1] = 29; // うるう年
+	}
+	getYoubi(year: number, month: number, day: number): number {
+		this.changeLeapYear(year);
+		const d = new Date(year, month - 1, day);
+		return d.getDay();
+	}
+	getMonthDays(year: number, month: number): number[] {
+		this.changeLeapYear(year);
+		/*@ts-ignore*/
+		return [...Array(this.monthDays[month - 1])].map((_, i) => i + 1);
+	}
+	getMonthLength(from: number, to: number, year: number): number {
+		this.changeLeapYear(year);
+		if (from > to) throw new Error("from is bigger than to");
+		if (from < 1 || from > 12) throw new Error("from is out of range");
+		let monthLength = 0;
+		for (let i = from - 1; i < to; i++) {
+			monthLength += this.monthDays[i];
+		}
+		return monthLength;
+	}
+	getMonthDaysLengths(year: number): number[] {
+		this.changeLeapYear(year);
+		return this.monthDays;
+	}
+}
+
+export default calendar;
---
+++ src/h_diaryHonshi/diaryPageStructure.ts
@@ -0,0 +1,50 @@
+export class diaryPageStructure {
+	dayStructureArray: diaryDayStructure[];
+	monthText: string;
+	monthEnglishText: string;
+	sengetsuText: string;
+	constructor(dayStructureArray: diaryDayStructure[]) {
+		const first = dayStructureArray[0];
+		const last = dayStructureArray[6];
+		const isMonthTextDifferent = first.monthText !== last.monthText ? true : false;
+		const isLastGlay = last.isGlay ? true : false;
+		const isSengetsu = isMonthTextDifferent && !isLastGlay ? true : false;
+		this.dayStructureArray = dayStructureArray;
+		this.monthText = !isLastGlay ? last.monthText : dayStructureArray[0].monthText;
+		this.monthEnglishText = !isLastGlay ? this.getEng(last.monthText) : this.getEng("3");
+		this.sengetsuText = isSengetsu ? first.monthText + "/" : "";
+	}
+	getEng(string: string) {
+		const monthEnglishTextList: { [key: string]: string } = {
+			"1": "January",
+			"2": "February",
+			"3": "March",
+			"4": "April",
+			"5": "May",
+			"6": "June",
+			"7": "July",
+			"8": "August",
+			"9": "September",
+			"10": "October",
+			"11": "November",
+			"12": "December",
+		};
+		return monthEnglishTextList[string];
+	}
+}
+
+export class diaryDayStructure {
+	monthText: string;
+	youbiText: string;
+	holidayText: string;
+	isGlay: boolean;
+	constructor(data: string[]) {
+		this.monthText = data[2];
+		this.youbiText = data[6];
+		this.holidayText = data[7];
+		this.isGlay = false;
+	}
+	setGlayActivate() {
+		this.isGlay = true;
+	}
+}
