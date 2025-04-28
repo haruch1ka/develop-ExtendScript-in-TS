eff5871f250ba82c247c6e3e7eae9f80d57827ea
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-04-28 16:03:02 +0900

    README.mdの内容を更新し、Polyfillのtypesを追加

---
+++ src/types/Polyfill.d.ts
@@ -0,0 +1,12 @@
+declare global {
+	interface Array<T> {
+		/**
+		 * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+		 * @param callback A function that accepts up to three arguments. The `map` method calls the callback function one time for each element in the array.
+		 * @param thisArg An object to which the `this` keyword can refer in the callback function. If `thisArg` is omitted, undefined is used as the `this` value.
+		 */
+		map<U>(callback: (value: T, index: number, array: T[]) => U, thisArg?: any): U[];
+	}
+}
+
+export {};
