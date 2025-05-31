7fd892cdf1ad4d276c2e23a96362dc216ae09353
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-05-31 16:51:02 +0900

    add BOM support in Rollup config and enable BOM emission in TypeScript config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
+function addBOM() {
+	return {
+		name: "add-bom",
+		generateBundle(options, bundle) {
+			for (const file of Object.values(bundle)) {
+				if (file.type === "chunk" && typeof file.code === "string") {
+					file.code = "\uFEFF" + file.code;
+				}
+			}
+		},
+	};
+}
+
 export default {
 	input: "compiled/index.js",
 	output: {
 		dir: "dist",
 		format: "esm",
 	},
+	plugins: [addBOM()],
 };
