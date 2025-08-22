04355eabe002e121fe65b74cd51ca41e9097a21d
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-08-22 13:16:16 +0900

    feat: update TypeScript configuration and add Vite build setup
    - Changed module target in tsconfig.json from "es2015" to "esnext".
    - Created a new .env file for environment variables.
    - Added targetFileList.ts to define the target files for the build process.
    - Implemented Vite configuration in vite.config.ts to build the project with BOM and output settings.

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+function addBOM() {
+	return {
+		name: "add-bom",
+		generateBundle(options, bundle) {
+			for (const file of Object.values(bundle)) {
+				if (file.type === "chunk" && typeof file.code === "string") {
+					file.code = "\uFEFF" + file.code;
+				}
+			}
+			console.log("\u001b[1;1m utf-8 BOM を追加。\u001b[0m");
+		},
+	};
+}
+
+export default {
+	build: {
+		target: "esnext", // ES2015をターゲットにする
+		emptyOutDir: false,
+		rollupOptions: {
+			input: `${process.env.TARGET_FILE}`,
+			output: {
+				dir: "dist", // 「dist」ディレクトリーの下にビルド後のファイルを生成する
+				entryFileNames: "[name].js", // 生成物のファイル名は input のキー名とする
+				// 今回は「desktop.js」というファイルが生成される
+			},
+		},
+	},
+	plugins: [addBOM()],
+};
