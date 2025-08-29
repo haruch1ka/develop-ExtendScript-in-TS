c786ba654b36534daff029c3e59427a866610c9f
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-08-29 09:56:15 +0900

    feat: targetFileListから要素を削除し、removeSpotColor関数を新規作成

--- targetFileList.ts
+++ targetFileList.ts
@@ -1,2 +1,2 @@
-const targetFileList = ["createAsset", "createAsset2", "InlineAnchorRemove", "q_tenPitter", "selectToConvertOutline"];
+const targetFileList = ["createAsset", "createAsset2", "テキストフレームアンカー解除", "句点ピッタリ", "テキストフレームをアウトライン化"];
 export default targetFileList;
