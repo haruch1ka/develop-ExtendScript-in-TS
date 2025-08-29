fb5ae29bb8a63e3c56be92ae23615bab48d8f9f2
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-08-29 09:56:15 +0900

    feat: targetFileListから要素を削除し、removeSpotColor関数を新規作成

---
+++ src/_util/removeSpotColor.ts
@@ -0,0 +1,27 @@
+export const removeSpotColor = (doc, targetColorName: Array<string>, showDialog = true) => {
+	const items = doc.pages[0].allPageItems;
+	const removedTextFrame = [];
+	for (let i = 0; i < items.length; i++) {
+		const item = items[i];
+		if (item.constructor.name === "TextFrame") {
+			const isRemoved = remove(item, targetColorName);
+			if (isRemoved) removedTextFrame.push(item);
+		}
+	}
+	removedTextFrame.map((item) => {
+		const isSpotColorOrNone = [...targetColorName, "None"].includes(item.strokeColor.name);
+		if (isSpotColorOrNone) item.remove();
+	});
+};
+
+const remove = (pageItem, targetColorName) => {
+	const characters = pageItem.characters;
+	if (!characters) return false;
+	for (let j = 0; j < characters.length; j++) {
+		if (targetColorName.includes(characters[j].fillColor.name)) {
+			characters[0].parentStory.remove();
+			return true;
+		}
+	}
+	return false;
+};
