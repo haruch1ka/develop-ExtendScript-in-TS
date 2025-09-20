03f37b8c1973b261bc94b18ef8bc1199c4ca791e
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-09-20 11:21:11 +0900

    fix: targetFileListの内容を「分数変換」に更新

--- config/rollup.config.ts
+++ config/rollup.config.ts
@@ -1,10 +1,10 @@
 import addBOM from "./addBOM.ts";
 export default {
-	input: `${process.env.TARGET_FILE}`,
-	output: {
-		dir: "dist", // 「dist」ディレクトリーの下にビルド後のファイルを生成する
-		entryFileNames: "[name].js", // 生成物のファイル名は input のキー名とする
-		// 今回は「desktop.js」というファイルが生成される
-	},
-	plugins: [addBOM()],
+  input: `${process.env.TARGET_FILE}`,
+  output: {
+    dir: "dist", // 「dist」ディレクトリーの下にビルド後のファイルを生成する
+    entryFileNames: "[name].js", // 生成物のファイル名は input のキー名とする
+    // 今回は「desktop.js」というファイルが生成される
+  },
+  plugins: [addBOM()],
 };
