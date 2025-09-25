a5b6b941710f520ef8fd83065c89fe1ed26eb9e0
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-09-25 16:15:05 +0900

    fix: removeGuide関数を削除し、targetFileListを更新

--- src/_util/removeGuide.ts
+++
@@ -1,8 +0,0 @@
-export const removeGuide = () => {
-  const guides = [] as any[];
-  const doc = app.activeDocument;
-  for (let i = 0; i < doc.guides.length; i++) {
-    const item = doc.guides[i];
-    item.remove();
-  }
-};
