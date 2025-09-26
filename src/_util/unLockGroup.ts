12587f2d2cb0a0399d681342801aa07a0d442289
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-09-26 16:13:41 +0900

    fix: launch.jsonのhostAppSpecifierをindesign-17.064に更新、targetFileListを修正しunLockGroup関数を追加

---
+++ src/_util/unLockGroup.ts
@@ -0,0 +1,23 @@
+export const unLockGroup = () => {
+  var doc = app.activeDocument;
+  var allPageItems = doc.pages[0].pageItems;
+  for (var i = 0; i < allPageItems.length; i++) {
+    const item = allPageItems[i].getElements()[0];
+    if (item.constructor.name == "Group") {
+      allPageItems[i].select(SelectionOptions.ADD_TO);
+    }
+  }
+  const selection = app.activeDocument.selection as any[];
+  $.writeln("selection.length:" + selection.length);
+
+  /*@ts-ignore */
+  const selGroup = doc.groups.add(doc.selection as any);
+  IrekoGroupKaijo(selGroup);
+  function IrekoGroupKaijo(GObj: any) {
+    while (GObj.groups.length > 0) {
+      //グループを破壊するのでダルマ落とし的に次のグループがgroups[0]に
+      $.writeln("ungroup");
+      GObj.groups[0].ungroup();
+    }
+  }
+};
