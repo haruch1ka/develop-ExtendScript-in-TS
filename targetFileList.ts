50251e7c38937aca951585b87cf8c04c5b58c4af
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-10-06 09:31:28 +0900

    feat: targetFileListにcreateAsset1とcreateAsset2を追加

--- targetFileList.ts
+++ targetFileList.ts
@@ -1,3 +1,3 @@
 // const targetFileList = ["removeSpotColor", "createAsset", "kuten", "removeAnchor","outline"];
-const targetFileList = ["createAsset_tashimato", "textFrameBarashi_tashimato", "removeSpotColor", "brakeTextFrame"];
+const targetFileList = ["createAsset_tashimato", "createAsset1", "createAsset2", "textFrameBarashi_tashimato", "removeSpotColor", "brakeTextFrame"];
 export default targetFileList;
