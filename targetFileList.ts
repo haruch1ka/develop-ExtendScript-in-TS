a5642bda91af3c5a979ce75ffb01c9394c8f5d14
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-10-17 13:53:07 +0900

    feat: targetFileListから不要なエントリを削除し、整理を行った

--- targetFileList.ts
+++ targetFileList.ts
@@ -1,17 +1,14 @@
 // const targetFileList = ["removeSpotColor", "createAsset", "kuten", "removeAnchor","outline"];
 const targetFileList = [
-  "brakeTextFrame",
-  "change_outline",
-  "createPng_omote",
-  "createPng_ura",
-  "createPng_tashimato",
-  "removeBlank",
-  "resize_kuten",
-  "resize_output",
-  "textFrameBarashi_tashimato",
-  //general
-  "create_blank",
+  //convert
+  "textFrameBarashi_kaigyo",
   "create_kakeami",
   "removeAnchor",
+  // duplicate
+  "createTextFrame_blank",
+  // insideArrangement
+  "removeSpotColor",
+  //other
+  "colorChanger",
 ];
 export default targetFileList;
