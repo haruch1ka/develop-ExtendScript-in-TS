3fc08c6e65d25fa0d454564372dcd0d0d98a8ff2
d-ito1106 <d_ito@taiho-pp.co.jp>
2026-07-13 10:16:35 +0900

    feat: 新しい設定ファイルとドキュメントを追加し、プロジェクトの概要を明確化

--- src/_scripts_sample/sample.ts
+++ src/_scripts_sample/sample.ts
@@ -7,37 +7,37 @@ import { formatText } from "./../_props/TextFrameWrapper";
 Polyfill();
 
 class Selection {
-	length: number = 0;
-	type: string = "";
-	constructor() {
-		const obj = <object[]>app.activeDocument.selection;
-		this.length = obj.length;
-		this.type = obj.length > 0 ? obj[0].constructor.name : "";
-	}
+  length: number = 0;
+  type: string = "";
+  constructor() {
+    const obj = <object[]>app.activeDocument.selection;
+    this.length = obj.length;
+    this.type = obj.length > 0 ? obj[0].constructor.name : "";
+  }
 }
 
 ///選択したテキストフレームにダイアログで入力したテキストを流し込むスクリプト
 
 const main = () => {
-	///////////////////////////
-	// データの取得
-	///////////////////////////
-	const s = new Selection();
-	if (s.length <= 0) return alert("何も選択されていません テキストフレームを選択してください");
-	if (s.length !== 1 || s.type !== "TextFrame") return alert("テキストフレームを1つ選択してください");
-	const dialog = new MyDialog("サンプルのダイアログ", "ラベルを入れられる :");
+  ///////////////////////////
+  // データの取得
+  ///////////////////////////
+  const s = new Selection();
+  if (s.length <= 0) return alert("何も選択されていません テキストフレームを選択してください");
+  if (s.length !== 1 || s.type !== "TextFrame") return alert("テキストフレームを1つ選択してください");
+  const dialog = new MyDialog("サンプルのダイアログ", "ラベルを入れられる :");
 
-	$.writeln("dialog.input1 : " + dialog.input1);
+  $.writeln("dialog.input1 : " + dialog.input1);
 
-	///////////////////////////
-	// メイン処理/データの流し込み
-	///////////////////////////
+  ///////////////////////////
+  // メイン処理/データの流し込み
+  ///////////////////////////
 
-	if (s.type === "TextFrame" && dialog.input1 !== "") {
-		/* @ts-ignore */
-		const textframe = app.activeDocument.selection[0] as TextFrame;
-		textframe.contents = dialog.input1;
-	}
+  if (s.type === "TextFrame" && dialog.input1 !== "") {
+    /* @ts-ignore */
+    const textframe = app.activeDocument.selection[0] as TextFrame;
+    textframe.contents = dialog.input1;
+  }
 };
 
 main();
